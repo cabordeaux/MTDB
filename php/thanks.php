acdb6eb3b8013a5cb357c7601b4dc1fc930ec8bd
Clovis Bordeaux <clovisbordeaux@gmail.com>
2018-04-16 10:12:10 -0500

    js lesson one

---
+++ php/thanks.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>thanks<title>
+   </head>
+
+   <body>
+       <h1>
+           "I appreciate you taking the time to contact me.  I will be in touch soon."
+       </h1>
